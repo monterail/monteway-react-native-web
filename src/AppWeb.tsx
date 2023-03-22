ed4d1f5c3907d2859723d81f3f04d659277425ec
hubertbialecki <hubert.bialecki@monterail.com>
2023-03-22 10:03:07 +0100

    Add vite and RN web config

---
+++ src/AppWeb.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function AppWeb() {
+  return (
+    <SafeAreaView>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default AppWeb;
