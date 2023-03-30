58152e12ec2b0d26f6fd0c212e6f4236971362ae
hubertbialecki <hubert.bialecki@monterail.com>
2023-03-30 15:06:22 +0200

    Adjust packages

--- src/AppMobile.tsx
+++ src/AppMobile.tsx
@@ -1,9 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import RNBootSplash from 'react-native-bootsplash';
 
 import AppStackMobile from '@/navigation/mobile/AppStackMobile';
+import { initSentry } from '@/services/sentry';
 
 function AppMobile(): JSX.Element {
+  useEffect(() => {
+    RNBootSplash.hide({ fade: true });
+    initSentry();
+  }, []);
   return (
     <NavigationContainer>
       <AppStackMobile />
